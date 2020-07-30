ac3e8f70261683263513a48c695377577eca6545
veaceslav.comarnitchi@gmail.com <veaceslav.comarnitchi@gmail.com>
2020-07-30 16:18:48 +0300

    Test publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.72"
-    "maven-publish"
 }
 
 group = "com.merax.livecalc"
--- src/androidTest/kotlin/com/merax/livecalc/OrderSplitInstrumentedTest.kt
+++ src/androidTest/kotlin/com/merax/livecalc/OrderSplitInstrumentedTest.kt
@@ -1,3 +1,4 @@
+/*
 package com.merax.livecalc
 
 import android.util.Log
@@ -12,11 +13,13 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+*/
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */
+ *//*
+
 @RunWith(AndroidJUnit4::class)
 class OrderSplitInstrumentedTest {
     @Test
@@ -42,3 +45,4 @@ class OrderSplitInstrumentedTest {
         Assert.assertEquals("com.merax.distributionapp", appContext.packageName)
     }
 }
+*/
