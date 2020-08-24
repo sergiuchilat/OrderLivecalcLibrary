2f8d1b29b1f33c112fdc9bcf26874dcea42b6b17
Sergiu Chilat <chilatsergiu@gmail.com>
2020-08-24 17:50:58 +0300

    Начал писать тест для применения скидки с типом n+m того же товара

---
+++ src/test/kotlin/com/merax/livecalc/DiscountSameProductUnitTest.kt
@@ -0,0 +1,13 @@
+package com.merax.livecalc
+
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+
+class DiscountSameProductUnitTest {
+    @Test
+    fun noDiscountsToApply() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
